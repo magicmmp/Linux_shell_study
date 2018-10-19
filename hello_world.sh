1e20006f937334d94d30c347f8bea6003d55c965
magicmmp <2233973404@qq.com>
2018-10-19 21:29:11 +0800

    add first file : hello_world.sh

---
+++ hello_world.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Hello World !"
+
+for skill in Ada Coffe Action Java; do
+    echo "I am good at ${skill}Script"
+done
+
+your_name="runoob"
+# 使用双引号拼接
+greeting="hello, "$your_name" !"
+greeting_1="hello, ${your_name} !"
+echo $greeting  $greeting_1
+# 使用单引号拼接
+greeting_2='hello, '$your_name' !'
+greeting_3='hello, ${your_name} !'
+echo $greeting_2  $greeting_3
