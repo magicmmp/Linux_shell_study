53ab6df5a7555d4fffe4936aa0da219d05998dec
magicmmp <2233973404@qq.com>
2018-10-20 14:10:20 +0800

    add some shell files .

---
+++ for.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+for i in $( seq 1 2 11 )
+do
+	echo "i= $i"
+done
---
+++ if_else.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+read -p "please input a,b:" a b
+if [ $a == $b ]
+then 
+	echo "$a 等于 $b"
+elif [ $a -gt $b  ]
+then
+	echo "$a 大于 $b"
+elif [ $a -lt $b  ]
+then 
+	echo "$a 小于 $b"
+else
+	echo "没有符合的条件"
+fi
+	 
